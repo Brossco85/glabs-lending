2ef1b267e6756ac79fbe2db241f04b151bbfc641
Bobby <Bobby1985uk@gmail.com>
2018-02-12 08:15:33 +0000

    added node-schedule job to execute job once per hour to fetch new bacs documents - job functionality to be added

--- server/server.js
+++ server/server.js
@@ -2,3 +2,9 @@ require('./config/config');
 
 const scheduler = require('node-schedule');
 
+const {retrieveBacsDocuments} = require('../jobs/RetrieveBacsDocuments');
+
+
+const scheduleFetchNewBacsJob = scheduler.scheduleJob('0 * * * *', () => {
+  retrieveBacsDocuments();
+});
