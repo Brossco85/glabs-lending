6b12e5f4f1755f56869e4329eeca29cce291f4b9
Bobby <Bobby1985uk@gmail.com>
2018-02-12 14:36:44 +0000

    fixed bug in server.js - mongoose had not been required

--- server/server.js
+++ server/server.js
@@ -1,10 +1,11 @@
 require('./config/config');
 
 const scheduler = require('node-schedule');
+const {mongoose} = require('./db/mongoose');
 
 const {retrieveBacsDocuments} = require('./jobs/retrieveBacsDocuments');
 
 
-const scheduleFetchNewBacsJob = scheduler.scheduleJob('0 * * * *', () => {
+const scheduleFetchNewBacsJob = scheduler.scheduleJob('34 * * * *', () => {
   retrieveBacsDocuments();
 });
