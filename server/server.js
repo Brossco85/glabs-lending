6b9f7fd207fba73c9ad803da14b193f489c1e59c
Bobby <Bobby1985uk@gmail.com>
2018-02-13 21:13:48 +0000

    added express and body parser dependancies, added dummy data to server test and get/returneddebit route to server file, route test passed

--- server/server.js
+++ server/server.js
@@ -1,11 +1,27 @@
 require('./config/config');
 
 const {CronJob} = require('cron');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 const {mongoose} = require('./db/mongoose');
+const {ReturnedDebit} = require('./models/returnedDebit');
+
 
 const {beginRetrieveNewBacsDocs} = require('./jobs/retrieveBacsDocuments');
 const {beginExportReturnedDebits} = require('./jobs/exportReturnedDebits');
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/returneddebits', (req, res) => {
+  ReturnedDebit.find().then((returnedDebits) => {
+    res.status(200).send({returnedDebits});
+  }, (err) => {
+    res.status(400).send(err);
+  });
+});
 
 
 new CronJob('0 21 * * * *', () => {
@@ -15,3 +31,9 @@ new CronJob('0 21 * * * *', () => {
 new CronJob('5 21 * * * *', () => {
   beginExportReturnedDebits();
 }, null, true, "Europe/London");
+
+app.listen(port, () => {
+  console.log(`Started on port ${port}`);
+});
+
+module.exports = {app};
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,6 +5,32 @@ const {ObjectId} = require('mongodb');
 const {app} = require('./../server');
 const {ReturnedDebit} = require('../models/returnedDebit');
 
+const returnedDebits = [{
+  _id: new ObjectId(),
+  originalAccountingRecord: {},
+  ref: "X01234",
+  transCode: "17",
+  returnCode: "1012",
+  valueOf: "65.00",
+  currency: "GBP",
+  payerAccount: {}
+},
+{
+  _id: new ObjectId(),
+  originalAccountingRecord: {},
+  ref: "X01235",
+  transCode: "18",
+  returnCode: "1013",
+  valueOf: "70.00",
+  currency: "GBP",
+  payerAccount: {}
+}];
+
+beforeEach((done) => {
+  ReturnedDebit.remove({}).then(() => {
+    return ReturnedDebit.insertMany(returnedDebits);
+  }).then(() => done());
+});
 
 describe('GET/returneddebits', () => {
 
@@ -13,7 +39,7 @@ describe('GET/returneddebits', () => {
     .get('/returneddebits')
     .expect(200)
     .expect((res) => {
-      expect(res.body.returneddebits.length).toBe(2);
+      expect(res.body.returnedDebits.length).to.equal(2);
     })
     .end(done);
   });
