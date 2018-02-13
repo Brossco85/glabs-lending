ff8f18ee2fe48ad0d2cc2ea58c141f08dbb550c0
Bobby <Bobby1985uk@gmail.com>
2018-02-13 20:46:37 +0000

    added basic tests for server get/returneddebits request, created boilerplate react app

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,20 +1,20 @@
-// const {expect} = require('chai');
-// // const request = require('supertest');
-// const {ObjectId} = require('mongodb');
+const {expect} = require('chai');
+const request = require('supertest');
+const {ObjectId} = require('mongodb');
 
-// const {app} = require('./../server');
-// const {ReturnedDebit} = require('../models/returnedDebit');
+const {app} = require('./../server');
+const {ReturnedDebit} = require('../models/returnedDebit');
 
 
-// describe('GET/returneddebits', () => {
+describe('GET/returneddebits', () => {
 
-//   it('Should return a list of all returned debits', (done) => {
-//     request(app)
-//     .get('/returneddebits')
-//     .expect(200)
-//     .expect((res) => {
-//       expect(res.body.returneddebits.length).toBe(2);
-//     })
-//     .end(done);
-//   });
-// });
+  it('Should return a list of all returned debits', (done) => {
+    request(app)
+    .get('/returneddebits')
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.returneddebits.length).toBe(2);
+    })
+    .end(done);
+  });
+});
