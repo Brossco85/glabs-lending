40f301ca5f544aa6383cd4813c5703dd88f3e2d6
Bobby <Bobby1985uk@gmail.com>
2018-02-13 22:58:54 +0000

    added concurrently dependacy to run client and server in sync, removed react router link from returned debits index as unused

--- client/src/components/returnedDebitsIndex.jsx
+++ client/src/components/returnedDebitsIndex.jsx
@@ -1,9 +1,8 @@
 import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { fetchReturnedDebits } from '../actions';
-import { Image, ListGroup, ListGroupItem, Badge } from 'react-bootstrap';
+import { ListGroup, ListGroupItem } from 'react-bootstrap';
 
 class ReturnedDebitsIndex extends Component {
 
